f623446360d961f3ee6b06ff24ddc48be75f10f3
Caxap <rebusiztysyachidetaley@bk.ru>
2017-01-13 00:41:19 +0400

    рабочий код

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
-
+import sys
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding='windows-1251') as json_content:
+        return json.load(json_content)
 
 
 def pretty_print_json(data):
-    pass
-
+    print(json.dumps(data, indent = 4, sort_keys = True, ensure_ascii=False))
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = sys.argv[1]
+    except IndexError:
+        print('Отсутствует путь к файлу. Программа будет завершена')
+        sys.exit()
+    json_content = load_data(filepath)
+    pretty_print_json(json_content)
